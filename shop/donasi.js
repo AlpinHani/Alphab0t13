7e3c99bccc4419ee411a41aad34de146b132d404
AlpinHani <99058202+AlpinHani@users.noreply.github.com>
2022-03-31 10:25:27 +0700

    Update donasi.js

--- shop/donasi.js
+++ shop/donasi.js
@@ -6,13 +6,13 @@ return `DONASI VIA E-MONEY
 *💰 E-MONEY*
 
 1. GOPAY
-	ㅁ 0882 3353 453
+	ㅁ 0822 8775 0102
 2. OVO
-	ㅁ 0882 3353 453
+	ㅁ 0822 8775 0102
 3. DANA
-	ㅁ 0882 3353 453
+	ㅁ 0822 8775 0102
 	
-Semuanya atas nama Ha***b
+Semuanya atas nama Al*in
 `
 	}
 
